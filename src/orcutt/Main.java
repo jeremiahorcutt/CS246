faa14478b1b0b6438f0bca5f816304ccfb086ab6
Emma Johns <joh19135@byui.edu>
2021-05-11 16:25:48 -0600

    Added team member's greeting

--- src/orcutt/Main.java
+++ src/orcutt/Main.java
@@ -3,6 +3,7 @@ package orcutt;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.print("Hello, my name is Jeremiah.");
+        System.out.print("Hello, my name is Jeremiah.");
+        System.out.print("Hello Jeremiah, my name is Emma!");
     }
 }
